766cebf1c761c85028ca3530c6ed054d49322e0b
Nabeel <nabeelismaeel74@gmail.com>
2024-05-02 12:41:55 +0300

    add Tokenizer

--- src/test/java/com/example/SearchEngine/SearchEngineApplicationTests.java
+++ src/test/java/com/example/SearchEngine/SearchEngineApplicationTests.java
@@ -1,12 +1,9 @@
 package com.example.SearchEngine;
 
-import com.example.SearchEngine.Tokenization.LowerCaseToken;
-import com.example.SearchEngine.Tokenization.Token;
-import com.example.SearchEngine.Tokenization.Tokenizer;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.List;
 
 @SpringBootTest
 class SearchEngineApplicationTests {
