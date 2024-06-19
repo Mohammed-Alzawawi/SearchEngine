3436346fae5706bc6d6f49f3c50440a19f8d1671
AhmdJaber <AljaberAhmad2003@gmail.com>
2024-06-20 02:36:02 +0300

    Optimizing the exception handling

--- src/main/java/com/example/SearchEngine/schema/service/SchemaFolderService.java
+++ src/main/java/com/example/SearchEngine/schema/service/SchemaFolderService.java
@@ -1,31 +1,40 @@
-package com.example.SearchEngine.directory;
+package com.example.SearchEngine.schema.service;
 
-import com.example.SearchEngine.path.PathController;
+import com.example.SearchEngine.utils.FileUtil;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-import java.io.IOException;
-
-
-public class FolderController {
+@Service
+public class SchemaFolderService {
     public static String path = "C:\\Search Engine\\Directory";
-    PathController pathController = new PathController();
-    ObjectMapper mapper = new ObjectMapper();
+    @Autowired
+    private SchemaPathService schemaPathService;
+    @Autowired
+    private ObjectMapper mapper;
 
-    public void createFolder(JsonNode jsonNode) throws IOException {
+    public void createFolder(JsonNode jsonNode) throws Exception {
         String folderPath = path + "\\" + jsonNode.get("id").toString();
-        if (FileUtilities.createFolder(folderPath)){
+        if (FileUtil.createFolder(folderPath)){
             System.out.println("Done creating the folder");
             String jsonFilePath = folderPath + "\\" + jsonNode.get("id").toString() + "_Schema.json";
-            String content = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(jsonNode);
-            if (FileUtilities.createFile(jsonFilePath, content)){
+            String content = null;
+            try {
+                content = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(jsonNode);
+            } catch (JsonProcessingException e) {
+                throw new IllegalArgumentException("Error writing json file");
+            }
+
+            if (FileUtil.createFile(jsonFilePath, content)){
                 System.out.println("Done creating the Json File");
             }
             else{
                 System.out.println("Problem with creating the Json File!");
             }
 
-            if (pathController.createPath(jsonNode.get("id").toString(), path)){
+            if (schemaPathService.createPath(jsonNode.get("id").toString(), path)){
                     System.out.println("Done creating the path file");
             }
             else {
@@ -37,10 +46,10 @@ public class FolderController {
         }
     }
 
-    public void deleteFolder(JsonNode jsonNode) throws IOException {
+    public void deleteFolder(JsonNode jsonNode) throws Exception {
         String folderPath = path + "\\" + jsonNode.get("id").toString();
-        if (FileUtilities.deleteFolder(folderPath)){
-            if (pathController.deletePath(jsonNode.get("id").toString())){
+        if (FileUtil.deleteFolder(folderPath)){
+            if (schemaPathService.deletePath(jsonNode.get("id").toString())){
                 System.out.println("Done deleting the folder");
             }
             else{
