c1a047c79d9d9f05450398d8bc673c144d724171
M7moud Ala3rj <ala3rjmo7@gmail.com>
2024-07-01 22:44:03 +0300

    storage service modified and linked with the schema controller

--- src/main/java/com/example/SearchEngine/utils/storage/JsonParserUtil.java
+++ src/main/java/com/example/SearchEngine/utils/storage/JsonParserUtil.java
@@ -1,4 +1,4 @@
-package com.example.SearchEngine.utils;
+package com.example.SearchEngine.utils.storage;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
