888b601ee3dc03b2293c9c1fd9fe6ce6353e1a4d
Mohammed Alzawawi <68351113+Mohammed-Alzawawi@users.noreply.github.com>
2024-07-18 21:50:31 +0300

    Update InvertedIndex.java

--- src/main/java/com/example/SearchEngine/invertedIndex/InvertedIndex.java
+++ src/main/java/com/example/SearchEngine/invertedIndex/InvertedIndex.java
@@ -6,7 +6,4 @@ public interface InvertedIndex {
     public void addDocument(String schemaName, Map<String, Object> document) throws Exception;
 
     public void deleteDocument(String schemaName, Integer documentId) throws Exception;
-
-    ;
-
 }
