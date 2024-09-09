bd4b18dba00df7066c4af90a8e59a19e9a194244
Nabeel <nabeelismaeel74@gmail.com>
2024-09-09 06:52:22 +0300

    build and validate search query

--- src/main/java/com/example/SearchEngine/document/service/Validation/FieldsValidations/DateValidation.java
+++ src/main/java/com/example/SearchEngine/document/service/Validation/FieldsValidations/DateValidation.java
@@ -4,20 +4,24 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 public class DateValidation implements FieldValidation {
+
+    private String format =  "dd/MM/yyyy" ;
     @Override
     public boolean validate(Object object) {
         String str = object.toString();
-        String[] formats = {"yyyy-MM-dd", "dd/MM/yyyy", "MM-dd-yyyy"};
-        for (String format : formats) {
-            try {
-                SimpleDateFormat sdf = new SimpleDateFormat(format);
-                sdf.setLenient(false);  // Set to false for stricter parsing
-                sdf.parse(str);
-                return true;
-            } catch (ParseException e) {
-                // Not valid in this format, continue looping
-            }
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat(format);
+            sdf.setLenient(false);
+            sdf.parse(str);
+            return true;
+        } catch (ParseException e) {
+
         }
         return false;
     }
+
+    public  String getFormat() {
+        return format;
+    }
+
 }
