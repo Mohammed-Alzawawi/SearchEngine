a76b4942ca7ebb4b7441d00ecaaa624d8f7b8b5a
Nabeel <nabeelismaeel74@gmail.com>
2024-12-13 19:12:13 +0300

    fix_update_and_trheadLocking

--- src/main/java/com/example/SearchEngine/document/service/DocumentStorageService.java
+++ src/main/java/com/example/SearchEngine/document/service/DocumentStorageService.java
@@ -17,10 +17,13 @@ import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 @Service
 public class DocumentStorageService {
 
+    private final ConcurrentHashMap<String, ReentrantReadWriteLock> lockMap = new ConcurrentHashMap<>();
     @Autowired
     SchemaPathService schemaPathService;
     @Autowired
@@ -36,54 +39,93 @@ public class DocumentStorageService {
     @Autowired
     private Snowflake snowflake;
 
-    public void addDocument(String schemaName, Map<String, Object> document) throws Exception {
-        if (documentValidator.validate(schemaName, document)) {
-            String path = schemaPathService.getSchemaPath(schemaName);
+    private ReentrantReadWriteLock getLock(String schemaName, Long documentId) {
+        return lockMap.computeIfAbsent(schemaName + ":" + documentId, s -> new ReentrantReadWriteLock());
+    }
 
-            long threadId = Thread.currentThread().getId();
-            Long id = snowflake.generate(threadId);
-            document.put("id", id);
+    public void addDocument(String schemaName, Map<String, Object> document) throws Exception {
+        if (!documentValidator.validate(schemaName, document)) {
+            throw new IllegalStateException("document not valid to schema");
+        }
+
+        long threadId = Thread.currentThread().getId();
+        Long id = snowflake.generate(threadId);
+        document.put("id", id);
+
+        JsonNode jsonNode = mapper.convertValue(document, JsonNode.class);
+        String path = schemaPathService.getSchemaPath(schemaName) + "documents/" + jsonNode.get("id").toString();
+        String content;
+        try {
+            content = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(jsonNode);
+        } catch (JsonProcessingException e) {
+            throw new IllegalArgumentException("Error writing json file");
+        }
+        if (CollectionInfo.isDocumentExist(schemaName, (Long) document.get("id"))) {
+            throw new IllegalStateException("this document already exists");
+        }
+
+        FileUtil.createFile(path, content);
+        trieInvertedIndex.addDocument(schemaName, document);
+        documentFilterService.addDocument(schemaName, (HashMap<String, Object>) document);
+        trieLogService.write(Command.INSERT, jsonNode.get("id").toString(), schemaName);
+    }
+
+    public void deleteDocument(String schemaName, Long documentId) throws Exception {
+        if (!CollectionInfo.isDocumentExist(schemaName, documentId)) {
+            throw new IllegalStateException("this document not exists");
+        }
+
+        Map<String, Object> document = getDocument(schemaName, documentId);
+        trieInvertedIndex.deleteDocument(schemaName, document);
+        documentFilterService.removeDocument(schemaName, (HashMap<String, Object>) document);
+        trieLogService.write(Command.DELETE, documentId.toString(), schemaName);
+    }
+
+
+    public void updateDocument(String schemaName, Long documentId, Map<String, Object> fields) throws Exception {
+        ReentrantReadWriteLock.WriteLock writeLock = getLock(schemaName, documentId).writeLock();
+        writeLock.lock();
+        try {
+            if (!documentValidator.updateValidation(schemaName, fields)) {
+                throw new IllegalStateException("not valid update");
+            }
+
+            Map<String, Object> document = getDocument(schemaName, documentId);
+            for (String fieldName : fields.keySet()) {
+                if (document.containsKey(fieldName)) {
+                    trieInvertedIndex.removeField(schemaName, document, fieldName);
+                }
+                document.put(fieldName, fields.get(fieldName));
+                trieInvertedIndex.addField(schemaName, document, fieldName);
+            }
+
             JsonNode jsonNode = mapper.convertValue(document, JsonNode.class);
-            path += "documents/" + jsonNode.get("id").toString();
+            String path = schemaPathService.getSchemaPath(schemaName) + "documents/" + jsonNode.get("id").toString();
+            FileUtil.deleteFile(path);
             String content;
             try {
                 content = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(jsonNode);
             } catch (JsonProcessingException e) {
                 throw new IllegalArgumentException("Error writing json file");
             }
-            if (CollectionInfo.isDocumentExist(schemaName , (Long)document.get("id"))) {
-                throw new IllegalStateException("this document already exists");
-            }
             FileUtil.createFile(path, content);
-            trieInvertedIndex.addDocument(schemaName, document);
-            documentFilterService.addDocument(schemaName, (HashMap<String, Object>) document);
-            trieLogService.write(Command.INSERT, jsonNode.get("id").toString(), schemaName);
-        } else {
-            throw new IllegalStateException("document not valid to schema");
+        } finally {
+            writeLock.unlock();
         }
     }
 
-    public void deleteDocument(String schemaName, Long documentId) throws Exception {
-        if (!CollectionInfo.isDocumentExist(schemaName , documentId) ) {
-            throw new IllegalStateException("this document not exists");
-        }
-
-        Map<String, Object> document = getDocument(schemaName, documentId);
-        trieInvertedIndex.deleteDocument(schemaName,document);
-        documentFilterService.removeDocument(schemaName, (HashMap<String, Object>) document);
-        trieLogService.write(Command.DELETE,documentId.toString(), schemaName);
-    }
-
-    public  void updateDocument(String schemaName, Long documentId, Map<String, Object> newDocument) throws Exception {
-        deleteDocument(schemaName, documentId);
-        addDocument(schemaName, newDocument);
-    }
 
     public Map<String, Object> getDocument(String schemaName, Long documentId) throws Exception {
-        String path = schemaPathService.getSchemaPath(schemaName);
-        path += "documents/" + documentId.toString();
-        String content = FileUtil.readFileContents(path);
-        Map<String, Object> document = mapper.readValue(content, Map.class);
-        return document;
+        ReentrantReadWriteLock.ReadLock readLock = getLock(schemaName, documentId).readLock();
+        readLock.lock();
+        try {
+            String path = schemaPathService.getSchemaPath(schemaName);
+            path += "documents/" + documentId.toString();
+            String content = FileUtil.readFileContents(path);
+            Map<String, Object> document = mapper.readValue(content, Map.class);
+            return document;
+        } finally {
+            readLock.unlock();
+        }
     }
 }
